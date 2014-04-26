ff870d7e859307cd0738b5b4460a47f0c12983fa
Lalo <lalo.sa.94@gmail.com>
2014-04-26 11:04:30 -0500

    Spiders WIP

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float maxSpeed = 0.1f;
+	public int timeMax = 2;
+	public bool turn; // Check whether it's the players' turn to move, if true
+	int direction;
+	int time;
+	Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		direction = 0;
+		turn = false;
+	}
+
+	public void startTurn() {
+		turn = true;
+	}
+
+	public bool getTurn() {
+		return turn;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rigidbody2D.velocity = new Vector2 (Mathf.Lerp(0, Input.GetAxis("Horizontal") * maxSpeed, 10.0f), Mathf.Lerp(0, Input.GetAxis("Vertical") * maxSpeed, 10.0f)) ;
+		if (direction == 0 && turn) {
+			bool moveUp = Input.GetKey (KeyCode.UpArrow);
+			bool moveDown = Input.GetKey (KeyCode.DownArrow);
+			bool moveLeft = Input.GetKey (KeyCode.LeftArrow);
+			bool moveRight = Input.GetKey (KeyCode.RightArrow);
+
+			if (moveUp) {
+				direction = 1;
+				time = timeMax;
+				anim.SetBool ("MoveUp", true);
+				anim.SetBool ("MoveDown", false);
+				anim.SetBool ("MoveLeft", false);
+				anim.SetBool ("MoveRight", false);
+			} else if (moveDown) {
+				direction = 2;
+				time = timeMax;
+				anim.SetBool ("MoveUp", false);
+				anim.SetBool ("MoveDown", true);
+				anim.SetBool ("MoveLeft", false);
+				anim.SetBool ("MoveRight", false);
+			} else if (moveLeft) {
+				direction = 3;
+				time = timeMax;
+				anim.SetBool ("MoveUp", false);
+				anim.SetBool ("MoveDown", false);
+				anim.SetBool ("MoveLeft", true);
+				anim.SetBool ("MoveRight", false);
+			} else if (moveRight) {
+				direction = 4;
+				time = timeMax;
+				anim.SetBool ("MoveUp", false);
+				anim.SetBool ("MoveDown", false);
+				anim.SetBool ("MoveLeft", false);
+				anim.SetBool ("MoveRight", true);
+			}
+		} else {
+			switch (direction) {
+				case 1:
+					rigidbody2D.velocity = new Vector2 (0f, maxSpeed);
+				break;
+				case 2:
+					rigidbody2D.velocity = new Vector2 (0f, -maxSpeed);
+				break;
+				case 3:
+					rigidbody2D.velocity = new Vector2 (-maxSpeed, 0f);
+				break;
+				case 4:
+					rigidbody2D.velocity = new Vector2 (maxSpeed, 0f);
+				break;
+			}
+
+			time--;
+			if (time <= 0) {
+				direction = 0;
+				turn = false;
+				rigidbody2D.velocity = new Vector2 (0f, 0f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SpiderScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderScript : EnemyScript {
+	int damage;
+	Animator anim;
+	public float maxSpeed = 2f;
+	int time;
+	bool down;
+	public int timeMax = 9;
+	// Use this for initialization
+	void Start () {
+		damage = 1;
+		agressive = false;
+		time = 0;
+		vision = 1;
+		direction = 2;
+		anim = GetComponent<Animator>();
+		turn = false;
+		down = false;
+	}
+
+	public void startTurn() {
+		turn = true;
+		Debug.Log("Spaidah turn: " + turn);
+	}
+	/*
+	void checkPlayer()
+	{ float monsterPositionX = this.transform.position.x;
+	  float monsterPositionY = this.transform.position.y;
+		if(GameObject.FindGameObjectWithTag("Player").transform.position.x<=monsterPositionX+tilesize*vision
+		   ||GameObject.FindGameObjectWithTag("Player").transform.position.x>=monsterPositionX-tilesize*vision)
+			agressive = true;
+		if(GameObject.FindGameObjectWithTag("Player").transform.position.y<=monsterPositionY+tilesize*vision
+		   ||GameObject.FindGameObjectWithTag("Player").transform.position.y>=monsterPositionY-tilesize*vision)
+			agressive = true;
+
+
+	} */
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (direction == 0 && turn) {
+			Debug.Log("Spaidah determining path to take");
+			Collider2D colUp = Physics2D.OverlapPoint(new Vector2(this.transform.position.x, this.transform.position.y + 0.32f));
+			Collider2D colDown = Physics2D.OverlapPoint(new Vector2(this.transform.position.x, this.transform.position.y - 0.32f));
+			if (anim.GetBool("SpiderUp")) {
+				if (colUp != null && colUp.CompareTag("Wall")) {
+					anim.SetBool("SpiderUp", false);
+					anim.SetBool("SpiderDown", true);
+					direction = 2;
+				} else {
+					direction = 1;
+					time = timeMax;
+				}
+			} else if (anim.GetBool("SpiderDown")) {
+				if (colDown != null && colDown.CompareTag("Wall")) {
+					anim.SetBool("SpiderUp", true);
+					anim.SetBool("SpiderDown", false);
+					direction = 1;
+				} else {
+					direction = 2;
+					time = timeMax;
+				}
+			}
+		}
+		else if (turn) {
+			Debug.Log("Spaidah moving to intercept");
+			switch (direction) {
+			case 1:
+				rigidbody2D.velocity = new Vector2 (0f, maxSpeed);
+				break;
+			case 2:
+				rigidbody2D.velocity = new Vector2 (0f, -maxSpeed);
+				break;
+			}
+
+			time--;
+			if (time <= 0) {
+				Debug.Log("Spaidah is done, for now");
+				direction = 0;
+				turn = false;
+				rigidbody2D.velocity = new Vector2 (0f, 0f);
+				GameObject dm = GameObject.Find("Dungeon Master");
+				DungeonMaster dmScript = (DungeonMaster) dm.GetComponent(typeof(DungeonMaster));
+				dmScript.notifyTurnFinish();
+			}
+		}
+	}
+}
