b1b62d388310b4bb2ee3f76a1ab508751a71d4d0
Ovi <oovi@outlook.com>
2014-05-26 12:41:26 -0500

    Version GameJam

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,7 @@ public class PlayerScript : MonoBehaviour {
 	public float maxSpeed = 0.1f;
 	public int timeMax = 2;
 	public bool turn; // Check whether it's the players' turn to move, if true
+	bool attacking;
 	/* Directions:
 	 * 0 = none
 	 * 1 = up
@@ -22,6 +23,7 @@ public class PlayerScript : MonoBehaviour {
 		anim = GetComponent<Animator> ();
 		direction = 0;
 		turn = false;
+		attacking = false;
 	}
 
 	public void startTurn() {
@@ -35,42 +37,92 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		//rigidbody2D.velocity = new Vector2 (Mathf.Lerp(0, Input.GetAxis("Horizontal") * maxSpeed, 10.0f), Mathf.Lerp(0, Input.GetAxis("Vertical") * maxSpeed, 10.0f)) ;
-		if (direction == 0 && turn) {
+		if (direction == 0 && turn && !attacking) {
+			Collider2D colUp = Physics2D.OverlapPoint(new Vector2(this.transform.position.x, this.transform.position.y + 0.32f));
+			Collider2D colDown = Physics2D.OverlapPoint(new Vector2(this.transform.position.x, this.transform.position.y - 0.32f));
+			Collider2D colLeft = Physics2D.OverlapPoint(new Vector2(this.transform.position.x - 0.32f, this.transform.position.y));
+			Collider2D colRight = Physics2D.OverlapPoint(new Vector2(this.transform.position.x + 0.32f, this.transform.position.y));
 			bool moveUp = Input.GetKey (KeyCode.UpArrow);
 			bool moveDown = Input.GetKey (KeyCode.DownArrow);
 			bool moveLeft = Input.GetKey (KeyCode.LeftArrow);
 			bool moveRight = Input.GetKey (KeyCode.RightArrow);
-
 			if (moveUp) {
-				direction = 1;
-				time = timeMax;
-				anim.SetBool ("MoveUp", true);
-				anim.SetBool ("MoveDown", false);
-				anim.SetBool ("MoveLeft", false);
-				anim.SetBool ("MoveRight", false);
-			} else if (moveDown) {
-				direction = 2;
-				time = timeMax;
-				anim.SetBool ("MoveUp", false);
-				anim.SetBool ("MoveDown", true);
-				anim.SetBool ("MoveLeft", false);
-				anim.SetBool ("MoveRight", false);
-			} else if (moveLeft) {
-				direction = 3;
-				time = timeMax;
-				anim.SetBool ("MoveUp", false);
-				anim.SetBool ("MoveDown", false);
-				anim.SetBool ("MoveLeft", true);
-				anim.SetBool ("MoveRight", false);
-			} else if (moveRight) {
-				direction = 4;
-				time = timeMax;
-				anim.SetBool ("MoveUp", false);
-				anim.SetBool ("MoveDown", false);
-				anim.SetBool ("MoveLeft", false);
-				anim.SetBool ("MoveRight", true);
+				if (colUp == null) {
+					direction = 1;
+					time = timeMax;
+					anim.SetBool ("MoveUp", true);
+					anim.SetBool ("MoveDown", false);
+					anim.SetBool ("MoveLeft", false);
+					anim.SetBool ("MoveRight", false);
+				}
+				else if (colUp.CompareTag("Spider")) {
+					attacking = true;
+					time = timeMax;
+					anim.SetBool("AttackUp", true);
+					anim.SetBool("AttackDown", false);
+					anim.SetBool("AttackLeft", false);
+					anim.SetBool("AttackRight", false);
+					//Destroy(colUp.transform.parent.gameObject);
+				}
 			}
-		} else {
+			else if (moveDown) {
+				if (colDown == null) {
+					direction = 2;
+					time = timeMax;
+					anim.SetBool ("MoveUp", false);
+					anim.SetBool ("MoveDown", true);
+					anim.SetBool ("MoveLeft", false);
+					anim.SetBool ("MoveRight", false);
+				}
+				else if (colDown.CompareTag("Spider")) {
+					attacking = true;
+					time = timeMax;
+					anim.SetBool("AttackUp", false);
+					anim.SetBool("AttackDown", true);
+					anim.SetBool("AttackLeft", false);
+					anim.SetBool("AttackRight", false);
+					//Destroy(colDown.transform.parent.gameObject);
+				}
+			}
+			else if (moveLeft) {
+				if (colLeft == null) {
+					direction = 3;
+					time = timeMax;
+					anim.SetBool ("MoveUp", false);
+					anim.SetBool ("MoveDown", false);
+					anim.SetBool ("MoveLeft", true);
+					anim.SetBool ("MoveRight", false);
+				}
+				else if (colLeft.CompareTag("Spider")) {
+					attacking = true;
+					time = timeMax;
+					anim.SetBool("AttackUp", false);
+					anim.SetBool("AttackDown", false);
+					anim.SetBool("AttackLeft", true);
+					anim.SetBool("AttackRight", false);
+					//Destroy(colLeft.transform.parent.gameObject);
+				}
+			}
+			else if (moveRight) {
+				if (colRight == null) {
+					direction = 4;
+					time = timeMax;
+					anim.SetBool ("MoveUp", false);
+					anim.SetBool ("MoveDown", false);
+					anim.SetBool ("MoveLeft", false);
+					anim.SetBool ("MoveRight", true);
+				}
+				else if (colRight.CompareTag("Spider")) {
+					attacking = true;
+					time = timeMax;
+					anim.SetBool("AttackUp", false);
+					anim.SetBool("AttackDown", false);
+					anim.SetBool("AttackLeft", false);
+					anim.SetBool("AttackRight", true);
+					//Destroy(colRight.transform.parent.gameObject);
+				}
+			}
+		} else if (turn) {
 			switch (direction) {
 				case 1:
 					rigidbody2D.velocity = new Vector2 (0f, maxSpeed);
@@ -90,6 +142,13 @@ public class PlayerScript : MonoBehaviour {
 			if (time <= 0) {
 				direction = 0;
 				turn = false;
+				if (attacking) {
+					anim.SetBool("AttackUp", false);
+					anim.SetBool("AttackDown", false);
+					anim.SetBool("AttackLeft", false);
+					anim.SetBool("AttackRight", false);
+				}
+				attacking = false;
 				rigidbody2D.velocity = new Vector2 (0f, 0f);
 			}
 		}
